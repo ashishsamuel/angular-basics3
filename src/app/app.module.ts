dfcf5e8fc9dedebabce643612d7d8d6b9c22cd6d
ashish <ashishsamuelthomson@gmail.com>
2025-04-08 23:29:54 +0530

    view encapsulation changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IfDirective } from 'src/directives/custom-directives/if.directive';
+import { Comp1Component } from './comp1/comp1.component';
+import { Comp2Component } from './comp2/comp2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IfDirective
+    IfDirective,
+    Comp1Component,
+    Comp2Component
   ],
   imports: [
     BrowserModule,
